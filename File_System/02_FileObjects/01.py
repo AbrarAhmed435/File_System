fe3d602396e54d7ed4fb33440a720bc78e8464d3
AbrarAhmed435 <abrar_2022bite057@nitsri.ac.in>
2025-11-03 23:44:23 +0530

    update

---
+++ File_System/02_FileObjects/01.py
@@ -0,0 +1,17 @@
+with open('../../../../password.txt','r') as f:
+    f_content=f.read()
+    print(f_content)
+
+
+
+
+
+
+f=open('../../../../password.txt','r')
+
+print(f.name)
+
+f.close()
+
+
+
